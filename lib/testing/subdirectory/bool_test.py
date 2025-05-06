f17384281980a2d3928971aa5704c944b1f0b4ea
edel <edel.omondi@student.moringaschool.com>
2025-05-06 20:09:28 +0300

    test lab complete

--- lib/testing/subdirectory/bool_test.py
+++ lib/testing/subdirectory/bool_test.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
-
 def return_true():
-    return True  # Correct implementation
+    return True 
 
 def test_return_true():
     '''in bool_functions, function "return_true" returns True.'''
